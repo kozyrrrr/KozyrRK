cd0aea11e6fcb4d0fb79c2a36f7745686cdf0603
kozyrrrr <kozyrstudy@gmail.com>
2022-05-24 21:53:58 +0300

    bad final
    upd

--- form.js
+++ form.js
@@ -10,3 +10,4 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     modal_container.classList.remove('show');
 });
+
--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
+
 const app = express();
+const port = 5500;
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/client/form.html`);
-});
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-app.listen(5500, () => {
-    console.log('Application listening on port 5500!');
-});
+
+
+app.get('/post', (req, res)=> {
+    res.render('form')
+})
+
+app.post('/form', urlencodedParser, [
+    check('nameInput', 'Нет имени')
+        .exists(),
+    check('quantityInput', 'Нет количества людей')
+        .exists()
+], (req, res)=> {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+       
+        const alert = errors.array()
+        res.render('form', {
+            alert
+        })
+    }
+})
+
+app.listen(port, () => console.info(`App listening on port: ${port}`))
